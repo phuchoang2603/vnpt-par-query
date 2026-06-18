2805974255c227f8427275c066c872ff6ff5f2c9
felix <xuanphuc.a1gv@gmail.com>
2026-06-18 17:28:02 +0700

    init uv
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -27,6 +27,10 @@
       enable = true;
       version = "3.12";
       venv.enable = true;
+      uv = {
+        enable = true;
+        sync.enable = true;
+      };
       lsp = {
         enable = true;
         package = pkgs.ty;
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from vnpt!")
+
+
+if __name__ == "__main__":
+    main()
